4a1a30c218c242298f25b3a08c68875fc0f89520
Jared Mingle <102331218+Retrodox@users.noreply.github.com>
2024-03-26 12:21:40 -0500

    Update RemoveName.ps1

--- bin/Debug/RemoveName.ps1
+++ bin/Debug/RemoveName.ps1
@@ -2,12 +2,16 @@ param(
     [int]$PrinterNumber
 )
 
-$printers = Get-Printer
+$printers = Get-WmiObject -Query "SELECT * FROM Win32_Printer"
 if ($PrinterNumber -le 0 -or $PrinterNumber -gt $printers.Count) {
     Write-Error "Invalid printer number."
     exit
 }
 
 $printerToRemove = $printers[$PrinterNumber - 1]
-Remove-Printer -Name $printerToRemove.Name
-Write-Output ("Removed printer: " + $printerToRemove.Name)
+$result = $printerToRemove.Delete()
+if ($result.ReturnValue -eq 0) {
+    Write-Output ("Removed printer: " + $printerToRemove.Name)
+} else {
+    Write-Output ("Failed to remove printer: " + $printerToRemove.Name + ". Error code: " + $result.ReturnValue)
+}
